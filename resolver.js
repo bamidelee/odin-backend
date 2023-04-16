4d44cd586f88bc2a4c169da757ea6c55af9a824f
bamidele <ibrahimolujide@gmail.com>
2023-04-16 14:26:24 +0100

    update

--- resolver.js
+++ resolver.js
@@ -243,7 +243,7 @@ const resolvers = {
                 type: 'movie'
             })
 
-            return   trend.sort((a,b) => b.trending.filter(d => d >= new Date(new Date().setDate(new Date().getDate() - 2))).length - a.trending.filter(d => d >= new Date(new Date().setDate(new Date().getDate() - 2))).length ).slice(0,10)
+            return   trend.sort((a,b) => b.trending.filter(d => d >= new Date(new Date().setDate(new Date().getDate() - 7))).length - a.trending.filter(d => d >= new Date(new Date().setDate(new Date().getDate() - 7))).length ).slice(0,10)
         },
 
 
